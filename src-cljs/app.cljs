edf2cfbb59f77408ca1a70222a7914c86a3a7934
Dmitriy Sorochenkov <podotkos@gmail.com>
2021-04-23 18:28:16 +0300

    Add form.

--- src-cljs/app.cljs
+++ src-cljs/app.cljs
@@ -1,6 +1,7 @@
 (ns testapp.app
   (:require [reagent.dom :as rdom]
-            [re-frame.core :refer [subscribe dispatch-sync]]
+            [reagent.core :as reagent]
+            [re-frame.core :refer [subscribe dispatch dispatch-sync]]
             [day8.re-frame.http-fx]
             [cljs.pprint :refer [pprint]]
             [testapp.events]))
@@ -20,8 +21,48 @@
       (for [request requests]
        ^{:key (:id request)} [request-item request])]))
 
+(defn request-form []
+  (let [default {:title "" :desc "" :reporter "" :assignee "" :date ""}
+        request (reagent/atom default)
+        post-request (fn [event request]
+                       (.preventDefault event)
+                       (dispatch [:post-request @request])
+                       )]
+    [:div 
+      [:h1 "New request"] 
+        [:form
+          [:fieldset
+            [:input {:type          "text"
+                     :placeholder   "Title"
+                     :default-value ""
+                     :on-change     #(swap! request assoc :title (-> % .-target .-value))}]]
+          [:fieldset
+            [:textarea {:rows          "4"
+                        :placeholder   "Description"
+                        :default-value ""
+                        :on-change     #(swap! request assoc :desc (-> % .-target .-value))}]]
+          [:fieldset
+            [:input {:type          "text"
+                     :placeholder   "Reporter"
+                     :default-value ""
+                     :on-change     #(swap! request assoc :reporter (-> % .-target .-value))}]]
+          [:fieldset
+            [:input {:type          "text"
+                     :placeholder   "Assignee"
+                     :default-value ""
+                     :on-change     #(swap! request assoc :assignee (-> % .-target .-value))}]]
+          [:fieldset
+            [:input {:type          "text"
+                     :placeholder   "Date"
+                     :default-value ""
+                     :on-change     #(swap! request assoc :date (-> % .-target .-value))}]]
+          [:button#form-button {:on-click #(post-request % request)} "Create request"]
+          ]]))
+
+
 (defn request-app []
  [:div 
+  [request-form]
   [requests-list]])
 
 (defn ^:export run []
